f9dde9dfe098b11e16d863a1d5e89985d5a3bebe
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2023-11-25 08:42:28 +0100

    feat(home): get and display photographers data

---
+++ scripts/pages/index.js
@@ -0,0 +1,20 @@
+async function getPhotographers() {
+	const response = await fetch('../../data/photographers.json');
+	return await response.json();
+}
+
+async function displayData(photographers) {
+	const photographersSection = document.querySelector('.photographer_section');
+
+	photographers.forEach((photographer) => {
+		const photographerModel = photographerTemplate(photographer);
+		const userCardDOM = photographerModel.getUserCardDOM();
+		photographersSection.appendChild(userCardDOM);
+	});
+}
+
+async function init() {
+	const { photographers } = await getPhotographers();
+	displayData(photographers);
+}
+init();
---
+++ scripts/templates/photographer.js
@@ -0,0 +1,41 @@
+function photographerTemplate(data) {
+	const { name, id, city, country, tagline, price, portrait } = data;
+
+	const picture = `assets/photographers/${portrait}`;
+
+	function getUserCardDOM() {
+		const article = document.createElement('article');
+		const img = document.createElement('img');
+
+		const link = document.createElement('a');
+		link.href = '#';
+
+		img.setAttribute('src', picture);
+		img.setAttribute('loading', 'lazy');
+
+		const h2 = document.createElement('h2');
+		h2.textContent = name;
+
+		const location = document.createElement('h3');
+		location.textContent = `${city}, ${country}`;
+		location.classList.add('location');
+
+		const tagLine = document.createElement('p');
+		tagLine.textContent = tagline;
+		tagLine.classList.add('tagline');
+
+		const tariff = document.createElement('span');
+		tariff.textContent = `${price}€/jour`;
+		tariff.classList.add('tariff');
+
+		article.appendChild(link);
+		link.appendChild(img);
+		link.appendChild(h2);
+		article.appendChild(location);
+		article.appendChild(tagLine);
+		article.appendChild(tariff);
+
+		return article;
+	}
+	return { data, getUserCardDOM };
+}
