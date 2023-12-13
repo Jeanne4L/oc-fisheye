7fec18f1bc4e6997e82a7f310c77778e14305829
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2023-12-13 11:15:43 +0100

    feat(media likes): increment count of likes on click

--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -14,7 +14,39 @@ export const mediaTemplate = (media, photographerName, isDisplayedInModal) => {
 		mediaTitle.textContent = title;
 		mediaTitle.classList.add('media-title');
 
-		const mediaLikes = document.createElement('span');
+		const mediaLikes = document.createElement('div');
+		mediaLikes.classList.add('media-likes');
+		const likesCount = document.createElement('span');
+		const likesIcon = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'svg'
+		);
+		likesIcon.setAttribute('viewBox', '0 -1 32 32');
+
+		const groupElement = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'g'
+		);
+		groupElement.setAttribute(
+			'transform',
+			'translate(-102.000000, -882.000000)'
+		);
+		groupElement.setAttribute('fill', '#901c1c');
+
+		const pathElement = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'path'
+		);
+		pathElement.setAttribute(
+			'd',
+			'M126,882 C122.667,882 119.982,883.842 117.969,886.235 C116.013,883.76 113.333,882 110,882 C105.306,882 102,886.036 102,890.438 C102,892.799 102.967,894.499 104.026,896.097 L116.459,911.003 C117.854,912.312 118.118,912.312 119.513,911.003 L131.974,896.097 C133.22,894.499 134,892.799 134,890.438 C134,886.036 130.694,882 126,882'
+		);
+		pathElement.setAttribute('id', 'heart-like');
+		pathElement.setAttribute('sketch:type', 'MSShapeGroup');
+
+		likesIcon.appendChild(groupElement);
+		groupElement.appendChild(pathElement);
+		likesIcon.addEventListener('click', incrementLikes);
 
 		article.appendChild(
 			createMedia(media, photographerName, isDisplayedInModal)
@@ -23,16 +55,14 @@ export const mediaTemplate = (media, photographerName, isDisplayedInModal) => {
 		mediaDescription.appendChild(mediaTitle);
 
 		if (!isDisplayedInModal) {
-			mediaLikes.innerHTML =
-				likes +
-				`<svg width='20px' height='20px' viewBox='0 -1 32 32' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:sketch='http://www.bohemiancoding.com/sketch/ns'>
-                    <g id='Page-1' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd' sketch:type='MSPage'>
-                        <g id='Icon-Set-Filled' sketch:type='MSLayerGroup' transform='translate(-102.000000, -882.000000)' fill='#901c1c'>
-                            <path d='M126,882 C122.667,882 119.982,883.842 117.969,886.235 C116.013,883.76 113.333,882 110,882 C105.306,882 102,886.036 102,890.438 C102,892.799 102.967,894.499 104.026,896.097 L116.459,911.003 C117.854,912.312 118.118,912.312 119.513,911.003 L131.974,896.097 C133.22,894.499 134,892.799 134,890.438 C134,886.036 130.694,882 126,882' id='heart-like' sketch:type='MSShapeGroup'></path>
-                        </g>
-                    </g>
-                </svg>`;
+			likesCount.textContent = likes;
+			mediaLikes.appendChild(likesCount);
+			mediaLikes.appendChild(likesIcon);
 			mediaDescription.appendChild(mediaLikes);
+		}
+
+		function incrementLikes() {
+			likesCount.textContent = likes + 1;
 		}
 
 		return article;
